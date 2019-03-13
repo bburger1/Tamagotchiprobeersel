5c80a595bc2d6e0d028a5ffc1aa3c93070b78668
nirvie <48092026+nirvie@users.noreply.github.com>
2019-03-13 11:41:03 +0100

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,10 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    }
+}
