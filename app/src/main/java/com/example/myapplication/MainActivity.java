05c466de5fbddd00aaa095eccd70b7f54e05ce58
nirvie <48092026+nirvie@users.noreply.github.com>
2019-03-21 19:24:06 +0100

    openstreet map erbij (alleen nog geen nette refrencie)

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,10 +1,21 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 
+    public void openMaps(View view){
+
+        Intent i = new Intent( this, maps.class);
+        startActivity(i);
     }
 }
