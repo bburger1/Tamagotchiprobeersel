b4e673684a756abd330446d1cdd217c312812025
nirvie <48092026+nirvie@users.noreply.github.com>
2019-03-26 20:32:49 +0100

    permissies werken en locatie is werkend. next step: icons zodat je het kan zien op de kaart

--- app/src/main/java/com/example/myapplication/maps.java
+++ app/src/main/java/com/example/myapplication/maps.java
@@ -1,37 +1,107 @@
 package com.example.myapplication;
+
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.preference.PreferenceManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+
+
 
 import org.osmdroid.api.IMapController;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.views.MapView;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.config.Configuration;
+import org.osmdroid.views.overlay.compass.CompassOverlay;
+import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
+import static android.content.ContentValues.TAG;
 
 
-public class maps extends Activity {
+public class maps extends Activity implements LocationListener {
     MapView map = null;
-    @Override public void onCreate(Bundle savedInstanceState) {
+
+    private MyLocationNewOverlay mLocationOverlay;
+    private CompassOverlay mCompassOverlay;
+    private Context context;
+    private static final String TAG = "maps";
+    private static final int LOCATION_REQUEST = 101;
+
+//    LocationRequest mLocationRequest;
+//    LocationClient mLocationClient;
+//    Location mCurrentLocation;
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         //handle permissions first, before map is created. not depicted here
 
         //load/initialize the osmdroid configuration, this can be done
-        Context ctx = getApplicationContext();
-        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+        context = getApplicationContext();
+        Configuration.getInstance().load(context, PreferenceManager.getDefaultSharedPreferences(context));
         //setting this before the layout is inflated is a good idea
         //it 'should' ensure that the map has a writable location for the map cache, even without permissions
         //if no tiles are displayed, you can try overriding the cache path using Configuration.getInstance().setCachePath
         //see also StorageUtils
         //note, the load method also sets the HTTP User Agent to your application's package name, abusing osm's tile servers will get you banned based on this string
 
+
+        //Check Permissions
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST);
+        }
+
         //inflate and create the map
         setContentView(R.layout.activity_maps);
 
         map = (MapView) findViewById(R.id.map);
         map.setTileSource(TileSourceFactory.MAPNIK);
+
+        // multi-touch zoom
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+
+        //default view point: eindhoven
+        IMapController mapController = map.getController();
+        mapController.setZoom(17.5);
+        GeoPoint Eindhoven = new GeoPoint(51.4415, 5.4721);
+        mapController.setCenter(Eindhoven);
+
+        //add my location
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+
+        final LocationManager locMgr = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        final String provider = locMgr.getBestProvider(criteria, true);
+        locMgr.requestLocationUpdates(provider, 1000, 100, this);
+
+        Location loc = locMgr.getLastKnownLocation(provider);
+        Log.d(TAG, "onCreate: " + loc.getLatitude() + ", " + loc.getLongitude());
+
+        //my location overlay
+        this.mLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(context),map);
+        this.mLocationOverlay.enableMyLocation();
+        map.getOverlays().add(this.mLocationOverlay);
+
+        // compass overlay
+        this.mCompassOverlay = new CompassOverlay(context, new InternalCompassOrientationProvider(context), map);
+        this.mCompassOverlay.enableCompass();
+        map.getOverlays().add(this.mCompassOverlay);
+
+
     }
 
     public void onResume(){
@@ -50,5 +120,25 @@ public class maps extends Activity {
         //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         //Configuration.getInstance().save(this, prefs);
         map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
